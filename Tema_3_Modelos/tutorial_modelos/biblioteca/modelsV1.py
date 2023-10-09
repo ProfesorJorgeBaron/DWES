c49da8339f490564b1b66e4291a748735043fd6f
Jorge Baron <jorge.baron@iespoligonosur.org>
2023-10-09 22:41:54 +0200

    Modelos y sus relaciones

--- Tema_3_Modelos/tutorial_modelos/biblioteca/modelsV1.py
+++ Tema_3_Modelos/tutorial_modelos/biblioteca/modelsV1.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Biblioteca(models.Model):
+    nombre = models.CharField(max_length=100)
+    direccion = models.TextField()
+
+class Autor(models.Model):
+    nombre = models.CharField(max_length=100)
+    apellidos = models.CharField(max_length=200,blank=True)
+    edad = models.IntegerField(null = True)
+    
 class Libro(models.Model):
     IDIOMAS = [
         ("ES", "Español"),
@@ -18,13 +28,14 @@ class Libro(models.Model):
     
     descripcion = models.TextField()
     fecha_publicacion = models.DateField()
-    
-class Autor(models.Model):
-    nombre = models.CharField(max_length=100)
-    apellidos = models.CharField(max_length=200,blank=True)
-    edad = models.IntegerField(null = True)
+
     
 class Cliente(models.Model):
     nombre = models.CharField(max_length=100)
     email = models.CharField(max_length=200,unique=True)
     puntos = models.FloatField(default=5.0,db_column = "puntos_biblioteca")
+    
+class DatosCliente(models.Model):
+     direccion = models.TextField()
+     gustos = models.TextField()
+     telefono = models.IntegerField()
