b91687015fe43556fcbb23b9174b3a88d1a3161b
Jorge Baron <jorge.baron@iespoligonosur.org>
2023-12-29 10:25:09 +0100

    Configuracion SECRET KEY

--- Tema_8_Despliegue/mysite/settings.py
+++ Tema_8_Despliegue/mysite/settings.py
@@ -19,13 +19,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
+import os
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n6c9_)%tpw4v$)%88vel+ut*la+=cyzgipmdr1t(okh4k3t4!e'
+SECRET_KEY =  os.environ["SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','0.0.0.0'] 
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com','0.0.0.0','192.168.1.179'] 
 
 INTERNAL_IPS = ["127.0.0.1"]
 
@@ -89,10 +90,21 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+'''
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+'''
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "OPTIONS": {
+            "service": "servicio_biblioteca",
+            "passfile": ".my_pgpass",
+        },
     }
 }
 
@@ -134,6 +146,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
