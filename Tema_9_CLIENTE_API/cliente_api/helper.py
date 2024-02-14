53818b7dcacc791731e470a47f9dd00eb2a3056d
Jorge Baron <jorge.baron@iespoligonosur.org>
2024-02-14 12:15:42 +0100

    Correccion cacheo en

--- Tema_9_CLIENTE_API/cliente_api/helper.py
+++ Tema_9_CLIENTE_API/cliente_api/helper.py
@@ -5,8 +5,9 @@ import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'),True)
 env = environ.Env()
-environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+
 
 class helper:
 
