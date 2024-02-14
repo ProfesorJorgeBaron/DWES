804c7deb6ee5435cd96601cbbfae5225680dc8c2
Jorge Baron <jorge.baron@iespoligonosur.org>
2024-02-14 13:10:14 +0100

    Refactorizacion

--- Tema_9_CLIENTE_API/cliente_api/helper.py
+++ Tema_9_CLIENTE_API/cliente_api/helper.py
@@ -78,3 +78,52 @@ class helper:
             return topMatch
         else:
             response.raise_for_status()
+            
+    
+    def realizar_peticion_api(url,metodo,datos=None,formulario=None):
+    
+        codigo = 0
+        try:
+            if metodo == "PUT" or metodo == "PATCH" or metodo == "POST":
+                headers = crear_cabecera_contenido()
+            else:
+                headers = crear_cabecera_token()
+            
+            if(datos is not None):
+                datos=json.dumps(datos)
+            
+            response = requests.put(
+                env("URL")+url,
+                headers=headers,
+                data=datos
+            )
+            codigo = response.status_code
+            response.raise_for_status()
+        except HTTPError as http_err:
+            print(f'Hubo un error en la petición: {http_err}')
+        except Exception as err:
+            print(f'Ocurrió un error: {err}')
+        return codigo
+        
+        
+    def mis_errores(code,formulario,url_respuesta,datosrespuesta):
+        if code == 400:
+            # Error de que la solicitud no pudo ser interpretada o estaba mal formada.
+            errores = response.json()
+            for error in errores:
+                formulario.add_error(error,errores[error])
+                return render(request, url_respuesta,datosrespuesta)
+        elif code == 401:
+            # Error de credenciales de auntenticación inválidas.
+            return mi_error_401(request) 
+        elif response.status_code == 403:
+            # Error de permisos de usuario.
+            return mi_error_403(request)
+        elif response.status_code == 404:
+            # Error de recurso no encontrado.
+            return mi_error_404(request)
+        else:
+            # Otros tipos de errores
+            return mi_error_500(request)
+
+        
--- Tema_9_CLIENTE_API/cliente_api/views.py
+++ Tema_9_CLIENTE_API/cliente_api/views.py
@@ -166,40 +166,22 @@ def libro_editar(request,libro_id):
             }
     )
     if (request.method == "POST"):
-        try:
-            formulario = LibroForm(request.POST)
-            headers = crear_cabecera()
-            datos = request.POST.copy()
-            datos["autores"] = request.POST.getlist("autores")
-            datos["categorias"] = request.POST.getlist("categorias")
-            datos["fecha_publicacion"] = str(datetime.date(year=int(datos['fecha_publicacion_year']),
-                                                        month=int(datos['fecha_publicacion_month']),
-                                                        day=int(datos['fecha_publicacion_day'])))
-           
-            response = requests.put(
-                'http://127.0.0.1:8000/api/v1/libros/editar/'+str(libro_id),
-                headers=headers,
-                data=json.dumps(datos)
-            )
-            if(response.status_code == requests.codes.ok):
-                return redirect("libro_mostrar",libro_id=libro_id)
-            else:
-                print(response.status_code)
-                response.raise_for_status()
-        except HTTPError as http_err:
-            print(f'Hubo un error en la petición: {http_err}')
-            if(response.status_code == 400):
-                errores = response.json()
-                for error in errores:
-                    formulario.add_error(error,errores[error])
-                return render(request, 
-                            'libro/actualizar.html',
-                            {"formulario":formulario,"libro":libro})
-            else:
-                return mi_error_500(request)
-        except Exception as err:
-            print(f'Ocurrió un error: {err}')
-            return mi_error_500(request)
+        formulario = LibroForm(request.POST)
+        headers = crear_cabecera()
+        datos = request.POST.copy()
+        datos["autores"] = request.POST.getlist("autores")
+        datos["categorias"] = request.POST.getlist("categorias")
+        datos["fecha_publicacion"] = str(datetime.date(year=int(datos['fecha_publicacion_year']),
+                                                    month=int(datos['fecha_publicacion_month']),
+                                                    day=int(datos['fecha_publicacion_day'])))
+        
+        
+        
+        codigo_respuesta = helper.realizar_peticion_api('editar/'+str(libro_id),"PUT",datos,formulario)
+        if(codigo_respuesta == 200):
+            return redirect("libro_mostrar",libro_id=libro_id)
+        else:
+            return helper.mis_errores(codigo_respuesta,formulario,'libro/actualizar.html',{"formulario":formulario,"libro":libro})
     return render(request, 'libro/actualizar.html',{"formulario":formulario,"libro":libro})
 
 
